1a2211678f7222fee0fadc35d9ec8a844c5beb58
Botir <tiboruz@gmail.com>
2024-12-18 11:14:06 +0500

    no logo

--- resources/views/price/show.blade.php
+++ resources/views/price/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.price')
 @section('content')
 <div class="container justify-center md align-middle" style="height:100vh">
-    <div class="justify-center">
+    {{--<div class="justify-center">
         <img src="images/logo.jpeg" alt="" style="width:50%;height:auto">
     </div>
-   
+   --}}
     <div class="card text-white bg-primary mt-1">
         <div class="card-header">{{$item->barcode}}</div>
         <div class="card-inner">
