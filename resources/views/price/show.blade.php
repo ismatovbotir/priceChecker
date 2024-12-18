f78cf86581f341c7326de4a96b0b54653238367c
Botir <tiboruz@gmail.com>
2024-12-18 11:10:59 +0500

    new design

--- resources/views/price/show.blade.php
+++ resources/views/price/show.blade.php
@@ -1,34 +1,24 @@
-
 @extends('layouts.price')
 @section('content')
+<div class="container justify-center md align-middle" style="height:100vh">
+    <div class="justify-center">
+        <img src="images/logo.jpeg" alt="">
+    </div>
+   
+    <div class="card text-white bg-primary mt-5">
+        <div class="card-header">{{$item->barcode}}</div>
+        <div class="card-inner">
+            <div class="m-5 d-flex justify-center">
 
-<div class="justify-center">
-    <img src="images/logo.jpeg" alt="">
-</div>
-{{--
-<form action="{{route('show')}}" method="POST">
-    @csrf
-    <label class="form-label" for="default-1-01">Barcode</label>
-    <input type="text" class="form-control focus" id="default-1-01" placeholder="" name="barcode" autofocus>
-</form>
-
-
---}}
-
-<div class="card text-white bg-primary mt-5">
-    <div class="card-header">{{$item->barcode}}</div>
-    <div class="card-inner">
-        <div class="m-5 d-flex justify-center">
-
-            <h2 class="card-title display-1">{{number_format($item->price,0,',',' ')}}</h2>
+                <h2 class="card-title display-1">{{number_format($item->price,0,',',' ')}}</h2>
+            </div>
+            <h3 class="display-5">{{$item->name}}</h3>
         </div>
-        <h3 class="display-5">{{$item->name}}</h3>
     </div>
 </div>
-
 @endsection
 
 @section('scripts')
-    @parent
-    <script src="{{asset('js/timer.js')}}"></script>
+@parent
+<script src="{{asset('js/timer.js')}}"></script>
 @endsection
